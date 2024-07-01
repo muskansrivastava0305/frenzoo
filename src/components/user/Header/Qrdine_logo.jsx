7ab8f834e146a2f99c4e76999dc3db5dfa5743b4
GODZ-k <tanmaykhatri07726@gmail.com>
2024-07-01 18:27:59 +0530

    update

--- src/components/user/Header/Qrdine_logo.jsx
+++ src/components/user/Header/Qrdine_logo.jsx
@@ -30,7 +30,7 @@ function Qrdine_logo() {
       <div>
       <div className=" h-[30vh] sm:h-[40vh] bg-no-repeat bg-cover bg-center px-3 sm:px-5 py-3 sm:py-4 flex-col bg-homeImage ">
         <div className=" w-full justify-center items-center flex">
-          <div className=" w-[10rem] sm:w-[13rem] flex justify-center items-center">
+          <div className=" w-[12rem] sm:w-[13rem] flex justify-center items-center">
             <img
               className=" h-full w-full"
               src="https://pindballuchi.com/wp-content/uploads/2024/02/Logo-1-removebg-preview.png"
@@ -38,7 +38,7 @@ function Qrdine_logo() {
             />
           </div>
         </div>
-        <div className=" pt-[6rem] sm:pt-[9rem] flex justify-center items-center">
+        <div className=" pt-[5rem] sm:pt-[9rem] flex justify-center items-center">
           <div className=" bg-[#ffffff1a] rounded-md w-[25rem] pt-2 px-3">
             <div className=" pb-4">
               <div className=" font-semibold text-[13px] sm:text-md text-white">Pind Balluchi</div>
