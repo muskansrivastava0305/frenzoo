b328fd8858475ca47634493831268a2f3d50d286
GODZ-k <tanmaykhatri07726@gmail.com>
2024-07-02 10:00:30 +0530

    update

--- src/components/user/Header/Qrdine_logo.jsx
+++ src/components/user/Header/Qrdine_logo.jsx
@@ -1,34 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 function Qrdine_logo() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    const updateMessage = () => {
-      const currentHour = new Date().getHours();
-
-      if (currentHour >= 9 && currentHour < 12) {
-        setMessage("Good Morning !");
-      } else if (currentHour >= 12 && currentHour < 15) {
-        setMessage("Good Afternoon !");
-      } else {
-        setMessage("Good Evening !");
-      }
-    };
-
-    updateMessage();
-
-    // Optionally, you can set an interval to update the message every minute
-    const intervalId = setInterval(updateMessage, 60000);
-
-    // Cleanup interval on component unmount
-    return () => clearInterval(intervalId);
-  }, []);
-
+ 
   return (
     <>
       <div>
-      <div className=" h-[30vh] sm:h-[40vh] bg-no-repeat bg-cover bg-center px-3 sm:px-5 py-3 sm:py-4 flex-col bg-homeImage ">
+      <div className=" relative h-[30vh] sm:h-[40vh] bg-no-repeat bg-cover bg-center px-3 sm:px-5 py-3 sm:py-4 flex-col bg-homeImage ">
         <div className=" w-full justify-center items-center flex">
           <div className=" w-[12rem] sm:w-[13rem] flex justify-center items-center">
             <img
@@ -38,14 +15,11 @@ function Qrdine_logo() {
             />
           </div>
         </div>
-        <div className=" relative pt-[5rem] sm:pt-[9rem] flex justify-center items-center">
-          <div className=" absolute top-[7rem] sm:top-[11.4rem] bg-[#ffffff1a] rounded-md w-[20rem] sm:w-[25rem] pt-2 px-3">
+        <div className=" sm:pt-[9rem] flex justify-center items-center">
+          <div className=" absolute bottom-0 bg-[#ffffff1a] rounded-md w-[20rem] sm:w-[25rem] pt-2 px-3">
             <div className=" pb-4">
               <div className=" font-semibold text-[13px] sm:text-md text-white">Pind Balluchi</div>
               <div className=" text-sm text-gray-400">Noida</div>
-            </div>
-            <div className=" text-3xl md:text-4xl pb-3  pt-3 sm:pt-6 text-orange-400 font-semibold text-center">
-              <div>{message}</div>
             </div>
           </div>
         </div>
--- src/pages/user/Home/Home_page.jsx
+++ src/pages/user/Home/Home_page.jsx
@@ -1,26 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Qrdine_logo } from "../../../components/user";
 
-function Home_page({children}) {
-  const [item , setItem] =  useState(false)
+function Home_page({ children }) {
+  const [item, setItem] = useState(false);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const updateMessage = () => {
+      const currentHour = new Date().getHours();
+
+      if (currentHour >= 9 && currentHour < 12) {
+        setMessage("Good Morning !");
+      } else if (currentHour >= 12 && currentHour < 15) {
+        setMessage("Good Afternoon !");
+      } else {
+        setMessage("Good Evening !");
+      }
+    };
+
+    updateMessage();
+
+    // Optionally, you can set an interval to update the message every minute
+    const intervalId = setInterval(updateMessage, 60000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-  <Qrdine_logo/>
-    {item && (<div className=" flex w-full justify-center">
-      <div className=" py-8 px-11 bg-white shadow-custom text-xl w-[37rem] text-center mt-20 rounded-3xl">
-        Please sacn the QR code first to access the main menu. thank you. 
+      <Qrdine_logo />
+      <div className=" text-3xl md:text-4xl pb-3  pt-3 sm:pt-4 text-orange-400 font-semibold text-center">
+        <div>{message}</div>
       </div>
-    </div>)}
+      {item && (
+        <div className=" flex w-full justify-center">
+          <div className=" py-8 px-11 bg-white shadow-custom text-xl w-[37rem] text-center mt-20 rounded-3xl">
+            Please sacn the QR code first to access the main menu. thank you.
+          </div>
+        </div>
+      )}  
 
-   <div className=" w-full justify-center flex px-4">
-  {!item && (
-     <div className=" mt-32 mb-[5rem] w-[34rem] lg:w-[40rem]">
-     {children}
+      <div className=" w-full justify-center flex px-4">
+        {!item && (
+          <div className=" mt-32 mb-[5rem] w-[34rem] lg:w-[40rem]">
+            {children}
+          </div>
+        )}
+      </div>
     </div>
-  )}
-   </div>
-  </div>
-  )
+  );
 }
 
 export default Home_page;
