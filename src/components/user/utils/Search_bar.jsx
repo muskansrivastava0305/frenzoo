47ced9581455865378843c3f3615c94d6c3b0162
GODZ-k <tanmaykhatri07726@gmail.com>
2024-07-03 13:24:21 +0530

    update

--- src/components/user/utils/Search_bar.jsx
+++ src/components/user/utils/Search_bar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Search_bar() {
   return (
-    <div className=" flex justify-center items-center -ml-[50px]">
-          <input className=" rounded-md w-fit px-2 py-1 border " type="text" placeholder="Search"/>
-          <button className="-m-7 text-gray-500 text-sm"><i className="fa-solid fa-magnifying-glass "></i></button>
+    <div className=" relative flex justify-center items-center ">
+          <div className=' relative'>
+            <input className=" rounded-md w-fit px-2 py-1 border " type="text" placeholder="Search"/>
+          <button className=" top-0 p-2 absolute right-0 text-gray-500 text-sm"><i className="fa-solid fa-magnifying-glass "></i></button>
+          </div>
       </div>
   )
 }
